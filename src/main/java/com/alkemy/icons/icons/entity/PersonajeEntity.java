4800a59baf087ed9c51af56790c7608c079cba7c
Luks <lucas.tapia@hotmail.com>
2022-08-25 16:19:32 -0300

    Unidad 1

---
+++ src/main/java/com/alkemy/icons/icons/entity/PersonajeEntity.java
@@ -0,0 +1,46 @@
+package com.alkemy.icons.icons.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "personaje")
+@Getter
+@Setter
+
+public class PersonajeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
+    private Long id;
+
+    private String imagen;
+
+    private String nombre;
+
+    private int edad;
+
+    private float peso;
+
+    private String historia;
+
+    @ManyToMany(mappedBy = "personajes", cascade = CascadeType.ALL)
+    private List<PeliculaEntity> peliculas = new ArrayList<>();
+
+    public void addPelicula(PeliculaEntity pelicula) {
+        this.peliculas.add(pelicula);
+    }
+    public void removePelicula(PeliculaEntity pelicula) {
+        this.peliculas.remove(pelicula);
+    }
+
+
+
+}
